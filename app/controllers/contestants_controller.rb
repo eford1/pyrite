1b0324c698e28ca66bd8c9c1250e4777ed7ee7e7
Elizabeth Ford <113866+eford1@users.noreply.github.com>
2023-10-10 19:10:40 -0700

    initial controller

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,4 +1,8 @@
 class ContestantsController < ApplicationController
+  # hard-coded for now
+  GOLDEN_BACHELOR_SEASON_ID = 1
+
   def index
+    @contestants = Contestant.where(season_id: GOLDEN_BACHELOR_SEASON_ID)
   end
 end
